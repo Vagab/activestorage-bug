46eb745355b2f11e133888d29cf58e41b88ef53a
barseek <sergey.b@hey.com>
2024-08-20 22:36:30 +0200

    comment

--- app/lib/test.rb
+++ app/lib/test.rb
@@ -1,6 +1,7 @@
 require "async/scheduler"
 
 module Test
+  # this fails
   def self.test_fails
     dummy = Dummy.first
     Fiber.set_scheduler Async::Scheduler.new
@@ -16,11 +17,13 @@ module Test
     Fiber.set_scheduler nil
   end
 
+  # this works
   def self.test_works
     dummy = Dummy.first
     Fiber.set_scheduler Async::Scheduler.new
     50.times do
       Fiber.schedule do
+        # notice the only difference with the above code - we instantiate the object again. So probably something with the instance variables
         Dummy.find(dummy.id).file.attach(content_type: "text/plain", filename: "dummy.txt", io: StringIO.new("dummy" * 100))
       end
       Fiber.schedule do
